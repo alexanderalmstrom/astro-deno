f76f2dee0c29f4e452db90c11eccca0bc6c3ae1c
Alexander Almström <info@alexanderalmstrom.com>
2025-02-27 00:42:17 +0100

    Get kv access token

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -2,7 +2,7 @@ import { DENO_KV_ACCESS_TOKEN, KV_UUID } from "astro:env/server";
 
 Deno.env.set(
   "DENO_KV_ACCESS_TOKEN",
-  Deno.env.get("DENO_KV_ACCESS_TOKEN") ?? DENO_KV_ACCESS_TOKEN,
+  Deno.env.get("KV_ACCESS_TOKEN") ?? DENO_KV_ACCESS_TOKEN,
 );
 
 const kv = await Deno.openKv(
