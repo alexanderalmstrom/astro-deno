f7ae5093d99a79407e8397a702f81e07e1b99eae
Alexander Almström <info@alexanderalmstrom.com>
2025-02-26 23:00:23 +0100

    Install tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import deno from "@deno/astro-adapter";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: deno(),
+  vite: {
+    // @ts-ignore - https://github.com/tailwindlabs/tailwindcss/discussions/15768
+    plugins: [tailwindcss()],
+  },
 });
