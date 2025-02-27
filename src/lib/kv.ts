0fc4946ef995ba6fb5fc4982b831753db4426768
Alexander Almström <info@alexanderalmstrom.com>
2025-02-27 01:13:30 +0100

    Check KV_ACCESS_TOKEN

--- src/lib/kv.ts
+++ src/lib/kv.ts
@@ -2,7 +2,7 @@ import { KV_ACCESS_TOKEN, KV_UUID } from "astro:env/server";
 
 const KV_URL = `https://api.deno.com/databases/${KV_UUID}/connect`;
 
-if (import.meta.env.DEV) {
+if (import.meta.env.DEV && KV_ACCESS_TOKEN) {
   Deno.env.set(
     "DENO_KV_ACCESS_TOKEN",
     KV_ACCESS_TOKEN,
